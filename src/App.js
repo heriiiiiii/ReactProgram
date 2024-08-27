9070b0be22ddcdef261ed7f7f550be74b97da071
heriiiiiii <vvh0033477@est.univalle.edu>
2024-08-26 23:34:15 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">     
+        <p>
+         Hola, mundo
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+    </div>
+  );
+}
+
+export default App;
